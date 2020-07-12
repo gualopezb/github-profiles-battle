c7b967f73270a19a436901f6dd15fae0c96f4a90
Gustavo López <gualopezb@gmail.com>
2020-07-11 22:25:33 -0500

    Show winner and loser profiles

--- app/components/Results.jsx
+++ app/components/Results.jsx
@@ -3,17 +3,42 @@ import React, { Component } from 'react';
 import { battle } from '../utils/api';
 
 export default class Results extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      winner: null,
+      loser: null,
+      error: null,
+      loading: true,
+    };
+  }
+
   componentDidMount() {
     // eslint-disable-next-line react/prop-types
     const { playerOne, playerTwo } = this.props;
 
-    battle([playerOne, playerTwo]).then(console.log);
+    battle([playerOne, playerTwo])
+      .then((players) => {
+        this.setState({
+          winner: players[0],
+          loser: players[1],
+          error: null,
+          loading: false,
+        });
+      })
+      .catch(({ message }) => {
+        this.setState({
+          error: message,
+          loading: false,
+        });
+      });
   }
 
   render() {
     return (
       <div>
-        Results<pre>{JSON.stringify(this.props, null, 2)}</pre>
+        Results<pre>{JSON.stringify(this.state, null, 2)}</pre>
       </div>
     );
   }
