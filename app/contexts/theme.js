c48a905c782ae2a17052580fed648f4c316a0890
Gustavo López <gualopezb@gmail.com>
2020-07-20 14:35:21 -0500

    Add theme using React context

---
+++ app/contexts/theme.js
@@ -0,0 +1,6 @@
+import { createContext } from 'react';
+
+const { Consumer, Provider } = createContext();
+
+export const ThemeConsumer = Consumer;
+export const ThemeProvider = Provider;
--- app/index.js
+++ app/index.js
@@ -2,18 +2,39 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 
-import Popular from './components/Popular';
+import { ThemeProvider } from './contexts/theme';
+
+// import Popular from './components/Popular';
 import Battle from './components/Battle';
+import Nav from './components/Nav';
 
 import './index.css';
 
-// eslint-disable-next-line react/prefer-stateless-function
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      theme: 'light',
+      toggleTheme: () => {
+        this.setState(({ theme }) => ({
+          theme: theme === 'light' ? 'dark' : 'light',
+        }));
+      },
+    };
+  }
+
   render() {
+    const { theme, toggleTheme } = this.state;
     return (
-      <div className="container">
-        <Battle />
-      </div>
+      <ThemeProvider value={{ theme, toggleTheme }}>
+        <div className={theme}>
+          <div className="container">
+            <Nav />
+            <Battle />
+          </div>
+        </div>
+      </ThemeProvider>
     );
   }
 }
