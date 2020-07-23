d174ab0ae02b103ef451f029f2a42f0552d33efa
Gustavo López <gualopezb@gmail.com>
2020-07-23 18:41:45 -0500

    Use react lazy and suspense for dynamic imports

--- app/index.js
+++ app/index.js
@@ -5,12 +5,14 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 import { ThemeProvider } from './contexts/theme';
 
-import Popular from './components/Popular';
-import Battle from './components/Battle';
-import Results from './components/Results';
 import Nav from './components/Nav';
+import Loading from './components/Loading';
 
 import './index.css';
+
+const Popular = React.lazy(() => import('./components/Popular'));
+const Battle = React.lazy(() => import('./components/Battle'));
+const Results = React.lazy(() => import('./components/Results'));
 
 class App extends Component {
   state = {
@@ -31,12 +33,14 @@ class App extends Component {
             <div className="container">
               <Nav />
 
-              <Switch>
-                <Route exact path="/" component={Popular} />
-                <Route exact path="/battle" component={Battle} />
-                <Route path="/battle/results" component={Results} />
-                <Route render={() => <h1>404</h1>} />
-              </Switch>
+              <React.Suspense fallback={<Loading />}>
+                <Switch>
+                  <Route exact path="/" component={Popular} />
+                  <Route exact path="/battle" component={Battle} />
+                  <Route path="/battle/results" component={Results} />
+                  <Route render={() => <h1>404</h1>} />
+                </Switch>
+              </React.Suspense>
             </div>
           </div>
         </ThemeProvider>
